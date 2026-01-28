92862a34ad0d30b762cbfc188f86c32dddbd5383
Jonathan <joujou0@gmail.com>
2026-01-28 17:30:37 -0300

    feat: progresso de aula 32_arrays

---
+++ javascript_2/32_arrays/array.js
@@ -0,0 +1,11 @@
+let listdaDeCompras = ["Macarrão"]
+
+listdaDeCompras[0] = "Arroz"
+listdaDeCompras[1] = "Feijão"
+listdaDeCompras[2] = "Óleo"
+listdaDeCompras[3] = 10
+listdaDeCompras[4] = true
+listdaDeCompras[5] = 5.8
+
+console.log(listdaDeCompras)
+console.log(listdaDeCompras[4])
