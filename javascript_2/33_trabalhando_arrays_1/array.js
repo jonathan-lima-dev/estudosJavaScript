147086a5ba2388585da6ad8dbd01eccaf1d72237
Jonathan de Lima Barroso <44059035+joujou0@users.noreply.github.com>
2026-01-29 08:01:35 -0300

    feat: continuação de aula sobre arrays

---
+++ javascript_2/33_trabalhando_arrays_1/array.js
@@ -0,0 +1,7 @@
+const arr = ["Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas", "Gimli"]
+
+//Add elementos: push
+arr.push("Boromir")
+console.log(arr)
+
+//Remover elementos: pop)
